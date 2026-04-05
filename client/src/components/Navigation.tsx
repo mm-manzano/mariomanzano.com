ca0487e5efa8a047856bef368d134d844f94fb99
mm-manzano <realtor@mariomanzano.com>
2026-04-05 06:49:57 -0500

    Update Navigation.tsx

--- client/src/components/Navigation.tsx
+++ client/src/components/Navigation.tsx
@@ -276,7 +276,9 @@ export default function Navigation(  ) {
                 window.fbq("track", "Contact");
               }
             }}
-            className="btn-luxury ..."
+            className={`text-[10px] py-2 ${
+            isSpanish ? "px-3" : "px-4"
+            } cursor-pointer border-0 bg-[#B8974A] border-[#B8974A] text-white hover:bg-[#9A7D3A] hover:border-[#9A7D3A]`}
           >
             {language === "es"
             ? "Iniciar una Conversación"

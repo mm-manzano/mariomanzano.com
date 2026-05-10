7e819cb0d4444468933c18c29764706f0aa84c26
mm-manzano <realtor@mariomanzano.com>
2026-05-10 13:37:53 -0500

    Update main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,10 +2,4 @@ import { ViteSSG } from 'vite-ssg/single-page';
 import App from './App';
 import './index.css';
 
-export const createApp = ViteSSG(
-  App,
-  ({ app, router, routes, isClient, initialState }) => {
-    // This function runs during both build and client-side hydration.
-    // You can add global plugins or analytics here if needed in the future.
-  }
-);
+export const createApp = ViteSSG(App);

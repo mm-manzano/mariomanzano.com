6a2fdde461f338880bbfde134a3e561e6412b76a
mm-manzano <realtor@mariomanzano.com>
2026-05-14 06:24:34 -0500

    Update HomeES.tsx

--- client/src/pages/HomeES.tsx
+++ client/src/pages/HomeES.tsx
@@ -209,7 +209,7 @@ export default function HomeES() {
         </div>
       </section>
 
-      {/* INTRODUCCIÓN DEL ASESOR */}}
+      {/* INTRODUCCIÓN DEL ASESOR */}
       <section className="py-20 md:py-32">
         <div className="container">
           <div className="grid grid-cols-1 lg:grid-cols-2 gap-12 lg:gap-20 items-center">
